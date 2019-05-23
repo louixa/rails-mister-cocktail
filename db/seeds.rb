9308ed93080972192ef01596341fcfacfbd2f40b
Louisa Wong <lou.h.wong@gmail.com>
2019-05-24 00:14:07 +0900

    lol

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+lemon_mojito = Cocktail.create!(name: 'Lemon Mojito')
+beach = Cocktail.create!(name: 'Sex On The Beach')
+love = Cocktail.create!(name: 'Hibiscus Love')
+
+lemon = Ingredient.create!(name: 'lemon')
+ice = Ingredient.create!(name: 'ice')
+mint_leaves = Ingredient.create!(name: 'mint leaves')
+
+Dose.create!(
+  description: '1 slice',
+  ingredient: lemon,
+  cocktail: lemon_mojito )
+
+Dose.create!(
+  description: '1 cup',
+  ingredient: ice,
+  cocktail: beach )
+
+Dose.create!(
+  description: '2 leaves',
+  ingredient: mint_leaves,
+  cocktail: love )
