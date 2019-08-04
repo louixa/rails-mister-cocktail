a0a5e2c534c37fb324bb547bc5260516178d7aee
Louisa Wong <lou.h.wong@gmail.com>
2019-08-05 02:58:47 +0900

    changed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,25 +8,35 @@
 Cocktail.destroy_all
 Ingredient.destroy_all
 
-lemon_mojito = Cocktail.create!(name: 'Lemon Mojito')
-beach = Cocktail.create!(name: 'Sex On The Beach')
-love = Cocktail.create!(name: 'Hibiscus Love')
+# lemon_mojito = Cocktail.create!(name: 'Lemon Mojito')
+# beach = Cocktail.create!(name: 'Sex On The Beach')
+# love = Cocktail.create!(name: 'Hibiscus Love')
 
-lemon = Ingredient.create!(name: 'lemon')
-ice = Ingredient.create!(name: 'ice')
-mint_leaves = Ingredient.create!(name: 'mint leaves')
+# lemon = Ingredient.create!(name: 'Lemon')
+# lime = Ingredient.create!(name: 'Lime')
+# ice = Ingredient.create!(name: 'Ice')
+# mint_leaves = Ingredient.create!(name: 'Mint leaves')
 
-Dose.create!(
-  description: '1 slice',
-  ingredient: lemon,
-  cocktail: lemon_mojito )
+# Dose.create!(
+#   description: '1 slice',
+#   ingredient: lemon,
+#   cocktail: lemon_mojito )
 
-Dose.create!(
-  description: '1 cup',
-  ingredient: ice,
-  cocktail: beach )
+# Dose.create!(
+#   description: '1 cup',
+#   ingredient: ice,
+#   cocktail: beach )
 
-Dose.create!(
-  description: '2 leaves',
-  ingredient: mint_leaves,
-  cocktail: love )
+# Dose.create!(
+#   description: '2 leaves',
+#   ingredient: mint_leaves,
+#   cocktail: love )
+
+red = Ingredient.create!(name: 'Red')
+black = Ingredient.create!(name: 'Black')
+white = Ingredient.create!(name: 'White')
+brown = Ingredient.create!(name: 'Brown')
+yellow = Ingredient.create!(name: 'Yellow')
+gray = Ingredient.create!(name: 'Gray')
+salt = Ingredient.create!(name: 'Salt & Pepper')
+multiple = Ingredient.create!(name: 'Muliple')
